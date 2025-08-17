c137664c55bfcd4ed8dfc5163971ce9f24a8bf0f
Tomas Navarro <tomas.navarro.m@outlook.com>
2025-08-17 01:34:54 -0400

    add: favoritos to test

--- backend/tests/server.spec.js
+++ backend/tests/server.spec.js
@@ -72,4 +72,39 @@ describe('DELETE /api/productos/', () => {
     });
 });
 
+// Favoritos
+describe('GET /api/favoritos/:id', () => {
+    it('should return status code 200 when fetching favoritos of a client', async () => {
+        const response = await request(app).get('/api/favoritos/1')
+        expect(response.status).toBe(201)
+    })
+
+    it("should return an array with at least one object", async () => {
+        const response = await request(app).get('/api/favoritos/1')
+        expect(Array.isArray(response.body)).toBe(false)   // body no debe ser un array
+        expect(typeof response.body[0]).toBe("object")    // y cada elemento debe ser objeto
+    })
+})
+
+describe('POST /api/favoritos', () => {
+    it('should create a new product and respond with status code 201', async () => {
+        const newFavorito = {
+            "cliente_id": 1,
+            "producto_id": 3
+        };
+        const response = await request(app)
+            .post('/api/favoritos')
+            .send(newFavorito);
+        expect(response.status).toBe(201);
+    });
+});
+
+describe('DELETE /api/favoritos/:id', () => {
+    it('should return status code 404 when trying to delete a product with a non-existent id', async () => {
+        const response = await request(app)
+            .delete('/api/favoritos/1')
+            .set('Authorization', 'test-token');
+        expect(response.status).toBe(200);
+    });
+});
 
